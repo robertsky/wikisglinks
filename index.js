9b6df351a7c86ecb84a3f3fd66dcf218e742b71f
robertsky <contact@robertsky.com>
2021-09-02 09:38:02 +0800

    some gen fixes

--- index.js
+++ index.js
@@ -54,10 +54,10 @@ fs.readFileAsync('category-list.txt', 'utf8').then(function(content) {
     console.log('Sorting...');
     outputArray = _.sortBy(_.uniq(outputArray), [function(o) {return o;}]);
     console.log('Writing to file...');
+    writeFile.write("{{short description|Wikimedia list article}}\n");
     writeFile.write("{{use Singapore English|date=August 2019}}\n");
     writeFile.write("{{use dmy dates|date=August 2019}}\n");
-    writeFile.write("{{short description|Wikimedia list article}}\n");
-    writeFile.write("This is a '''list of [[Singapore]]-related articles by alphabetical order'''. To learn quickly what Singapore is, see [[Outline of Singapore]]. Those interested in the subject can monitor changes to the pages by clicking on ''Related changes'' in the sidebar. A list of [[to do]] topics can be found [[Wikipedia:WikiProject_Singapore/Article_improvement|here]].\n\n");
+    writeFile.write("This is a '''list of [[Singapore]]-related articles by alphabetical order'''. To learn quickly what Singapore is, see [[Outline of Singapore]]. Those interested in the subject can monitor changes to the pages by clicking on ''Related changes'' in the sidebar. A list of [[to do]] topics can be found [[Wikipedia:WikiProject Singapore/Article improvement|here]].\n\n");
     writeFile.write("Articles related to '''[[Singapore]]''' include:\n");
     writeFile.write('{{alphanumeric TOC|numbers=yes|align=center}}\n');
     writeFile.write('==0-9==\n')
@@ -68,7 +68,7 @@ fs.readFileAsync('category-list.txt', 'utf8').then(function(content) {
         if (firstChar === headings.charAt(headingPosition)) {
             writeFile.write('{{div col end}}\n');
             writeFile.write('{{alphanumeric TOC|numbers=yes|align=center|top=yes}}\n');
-            writeFile.write('==' + headings.charAt(headingPosition) + '==\n');
+            writeFile.write('\n==' + headings.charAt(headingPosition) + '==\n');
             writeFile.write('{{div col|colwidth=25em}}\n');
             headingPosition++;
         }
